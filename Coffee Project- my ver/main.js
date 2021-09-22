f3a70fddef4b08c0b4bd6ee7c8604232eb07b18b
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2021-09-22 16:22:15 -0500

    Coffee Project Files (my ver)

---
+++ Coffee Project- my ver/main.js
@@ -0,0 +1,108 @@
+"use strict"
+
+/* Displays Coffees on page  */
+
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    html += '<div class="noId">' + coffee.id + '</div>';
+    html += '<h4>' + coffee.name + '</h4>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+
+    return html;
+}
+
+/* Loops through the coffees array  */
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+/* Bonus (Ignore for now)  */
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+/* Coffees Array  */
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+/* Variables to use later, target ids  */
+
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var roastSelection = document.querySelector('#roast-selection');
+
+tbody.innerHTML = renderCoffees(coffees.reverse());
+
+
+
+submitButton.addEventListener('click', updateCoffees);
+
+
+var search = document.getElementById("search");
+
+var input = document.querySelector('input[type="search"]');
+
+
+
+
+
+
+
+
+
+/* DORIAN ADVICE
+
+***Add function to input***
+
+Make new array and store each letter in empty array.
+
+Use a loop to loop through the array.
+
+Key up and Key down.
+
+
+ */
+
+
+/* First we need to make a loop that loops through the array (we have something
+for this already).
+
+Then we need to check whatever was in the search input and compare it to
+the array.
+
+THEN we need to update the results.
+
+ */
+
+

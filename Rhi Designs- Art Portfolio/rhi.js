4abdc1d5f6f0db2e7a271b09b3423c707e5fc772
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2021-10-24 18:27:23 -0500

    Art Portfolio - Art Added + Intersection Observer

--- Rhi Designs- Art Portfolio/rhi.js
+++ Rhi Designs- Art Portfolio/rhi.js
@@ -1,9 +1,20 @@
 console.log("Test");
 
-// var $start = $(".start");
-// var $ufo = $(".ufo");
-//
-// $Start.on("click", function () {
-//     $ufo.addClass(".ufo-move")
-// })
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const square = entry.target.querySelector('.ufo');
+        const text = entry.target.querySelector(".fading-effect")
 
+        if (entry.isIntersecting) {
+            square.classList.add('ufo-animation');
+            text.classList.add('fading-effect-animation');
+            return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        square.classList.remove('ufo-animation');
+        text.classList.remove('fading-effect-animation');
+    });
+});
+
+observer.observe(document.querySelector('.skills-title'));

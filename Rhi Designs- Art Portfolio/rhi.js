04c082c4b5865655c2cd46ae637f45ab5a6c5580
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2021-12-28 00:57:12 -0600

    AP - Added Info Icons for Flat Art

--- Rhi Designs- Art Portfolio/rhi.js
+++ Rhi Designs- Art Portfolio/rhi.js
@@ -67,3 +67,88 @@ function popup2() {
     var popup = document.getElementById("myPopup2");
     popup.classList.toggle("show");
 }
+
+function popup3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
+
+function popup4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+}
+
+function popup5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+}
+
+function popup6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+}
+
+function popup7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+}
+
+function popup8() {
+    var popup = document.getElementById("myPopup8");
+    popup.classList.toggle("show");
+}
+
+function popup9() {
+    var popup = document.getElementById("myPopup9");
+    popup.classList.toggle("show");
+}
+
+function popup10() {
+    var popup = document.getElementById("myPopup10");
+    popup.classList.toggle("show");
+}
+
+function popup11() {
+    var popup = document.getElementById("myPopup11");
+    popup.classList.toggle("show");
+}
+
+function popup12() {
+    var popup = document.getElementById("myPopup12");
+    popup.classList.toggle("show");
+}
+
+function popup13() {
+    var popup = document.getElementById("myPopup13");
+    popup.classList.toggle("show");
+}
+
+function popup14() {
+    var popup = document.getElementById("myPopup14");
+    popup.classList.toggle("show");
+}
+
+function popup15() {
+    var popup = document.getElementById("myPopup15");
+    popup.classList.toggle("show");
+}
+
+function popup16() {
+    var popup = document.getElementById("myPopup16");
+    popup.classList.toggle("show");
+}
+
+function popup17() {
+    var popup = document.getElementById("myPopup17");
+    popup.classList.toggle("show");
+}
+
+function popup18() {
+    var popup = document.getElementById("myPopup18");
+    popup.classList.toggle("show");
+}
+
+function popup19() {
+    var popup = document.getElementById("myPopup19");
+    popup.classList.toggle("show");
+}
